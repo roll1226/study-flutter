43a67002c5a51a398c33cfef9d4edd600d23bb6b
roll1226 <tomo_yasu_4724@icloud.com>
2022-04-05 21:33:12 +0900

    feat: pavlova

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 void main() {
-  debugPaintSizeEnabled = true; // Remove to suppress visual layout
+  debugPaintSizeEnabled = true; // Set to true for visual layout
   runApp(const MyApp());
 }
 
@@ -13,52 +13,249 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        // Change to buildColumn() for the other column example
-        body: Center(child: buildRow()),
-      ),
+      home: buildHomePage('Strawberry Pavlova Recipe'),
     );
   }
 
-  Widget buildRow() =>
-      // #docregion Row
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Expanded(
-            child: Image.asset('lib/images/sample2.jpeg'),
-          ),
-          Expanded(
-            child: Image.asset('lib/images/sample1.jpeg'),
-          ),
-          Expanded(
-            child: Image.asset('lib/images/sample3.jpeg'),
-          ),
-        ],
-      );
-  // #enddocregion Row
+  Widget buildHomePage(String title) {
+    const titleText = Padding(
+      padding: EdgeInsets.all(20),
+      child: Text(
+        'Strawberry Pavlova',
+        style: TextStyle(
+          fontWeight: FontWeight.w800,
+          letterSpacing: 0.5,
+          fontSize: 30,
+        ),
+      ),
+    );
 
-  Widget buildColumn() =>
-      // #docregion Column
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+    const subTitle = Text(
+      'Pavlova is a meringue-based dessert named after the Russian ballerina '
+      'Anna Pavlova. Pavlova features a crisp crust and soft, light inside, '
+      'topped with fruit and whipped cream.',
+      textAlign: TextAlign.center,
+      style: TextStyle(
+        fontFamily: 'Georgia',
+        fontSize: 25,
+      ),
+    );
+
+    // #docregion ratings, stars
+    var stars = Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        Icon(Icons.star, color: Colors.green[500]),
+        const Icon(Icons.star, color: Colors.black),
+        const Icon(Icons.star, color: Colors.black),
+      ],
+    );
+    // #enddocregion stars
+
+    final ratings = Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Expanded(
-            child: Image.asset('lib/images/sample2.jpeg'),
-          ),
-          Expanded(
-            child: Image.asset('lib/images/sample1.jpeg'),
-          ),
-          Expanded(
-            child: Image.asset('lib/images/sample3.jpeg'),
+          stars,
+          const Text(
+            '170 Reviews',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20,
+            ),
           ),
         ],
-      );
-  // #enddocregion Column
+      ),
+    );
+    // #enddocregion ratings
+
+    // #docregion iconList
+    const descTextStyle = TextStyle(
+      color: Colors.black,
+      fontWeight: FontWeight.w800,
+      fontFamily: 'Roboto',
+      letterSpacing: 0.5,
+      fontSize: 18,
+      height: 2,
+    );
+
+    // DefaultTextStyle.merge() allows you to create a default text
+    // style that is inherited by its child and all subsequent children.
+    final iconList = DefaultTextStyle.merge(
+      style: descTextStyle,
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Column(
+              children: [
+                Icon(Icons.kitchen, color: Colors.green[500]),
+                const Text('PREP:'),
+                const Text('25 min'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.timer, color: Colors.green[500]),
+                const Text('COOK:'),
+                const Text('1 hr'),
+              ],
+            ),
+            Column(
+              children: [
+                Icon(Icons.restaurant, color: Colors.green[500]),
+                const Text('FEEDS:'),
+                const Text('4-6'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+    // #enddocregion iconList
+
+    // #docregion leftColumn
+    final leftColumn = Container(
+      padding: const EdgeInsets.fromLTRB(20, 30, 20, 20),
+      child: Column(
+        children: [
+          titleText,
+          subTitle,
+          ratings,
+          iconList,
+        ],
+      ),
+    );
+    // #enddocregion leftColumn
+
+    final mainImage = Image.asset(
+      'lib/images/sample1.jpeg',
+      fit: BoxFit.cover,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      // #docregion body
+      body: Center(
+        child: Container(
+          margin: const EdgeInsets.fromLTRB(0, 40, 0, 30),
+          height: 600,
+          child: Card(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: leftColumn,
+                ),
+                // SizedBox(
+                //   width: 440,
+                //   child: leftColumn,
+                // ),
+                Expanded(
+                  child: mainImage,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      // #enddocregion body
+    );
+  }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+// void main() {
+//   debugPaintSizeEnabled = true; // Remove to suppress visual layout
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter layout demo',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Flutter layout demo'),
+//         ),
+//         // Change to buildColumn() for the other column example
+//         body: Center(child: buildRating()),
+//       ),
+//     );
+//   }
+
+//   Widget buildRow() =>
+//       // #docregion Row
+//       Row(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Expanded(
+//             child: Image.asset('lib/images/sample2.jpeg'),
+//           ),
+//           Expanded(
+//             child: Image.asset('lib/images/sample1.jpeg'),
+//           ),
+//           Expanded(
+//             child: Image.asset('lib/images/sample3.jpeg'),
+//           ),
+//         ],
+//       );
+//   // #enddocregion Row
+
+//   Widget buildColumn() =>
+//       // #docregion Column
+//       Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Expanded(
+//             child: Image.asset('lib/images/sample2.jpeg'),
+//           ),
+//           Expanded(
+//             child: Image.asset('lib/images/sample1.jpeg'),
+//           ),
+//           Expanded(
+//             child: Image.asset('lib/images/sample3.jpeg'),
+//           ),
+//         ],
+//       );
+//   // #enddocregion Column
+
+//   Widget buildRating() => Row(
+//         mainAxisSize: MainAxisSize.min,
+//         children: [
+//           Icon(Icons.star, color: Colors.green[500]),
+//           Icon(Icons.star, color: Colors.green[500]),
+//           Icon(Icons.star, color: Colors.green[500]),
+//           const Icon(Icons.star, color: Colors.black),
+//           const Icon(Icons.star, color: Colors.black),
+//         ],
+//       );
+
+//   var stars = Row(
+//     mainAxisSize: MainAxisSize.min,
+//     children: [
+//       Icon(Icons.star, color: Colors.green[500]),
+//       Icon(Icons.star, color: Colors.green[500]),
+//       Icon(Icons.star, color: Colors.green[500]),
+//       const Icon(Icons.star, color: Colors.black),
+//       const Icon(Icons.star, color: Colors.black),
+//     ],
+//   );
+// }
 
 // import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
