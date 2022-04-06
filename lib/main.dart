812e51ea8baaae7624d2f56cf0da260676302cbb
roll1226 <tomo_yasu_4724@icloud.com>
2022-04-06 22:20:59 +0900

    feat: grid

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,8 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  static const showGrid = true; // Set to false to show ListView
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,47 +19,116 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: Center(child: _buildImageColumn()),
+        body: Center(child: showGrid ? _buildGrid() : _buildList()),
       ),
     );
   }
 
-  // #docregion column
-  Widget _buildImageColumn() {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Colors.black26,
-      ),
-      child: Column(
-        children: [
-          _buildImageRow(1),
-          _buildImageRow(3),
-        ],
+  Widget _buildGrid() => GridView.extent(
+      maxCrossAxisExtent: 150,
+      padding: const EdgeInsets.all(4),
+      mainAxisSpacing: 4,
+      crossAxisSpacing: 4,
+      children: _buildGridTileList(30));
+
+  List<Container> _buildGridTileList(int count) => List.generate(
+      count, (i) => Container(child: Image.asset('images/pic$i.jpg')));
+
+  Widget _buildList() {
+    return ListView(
+      children: [
+        _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+        _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+        _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+        _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+        _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+            Icons.theaters),
+        _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+        const Divider(),
+        _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+        _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+        _tile('Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+        _tile('La Ciccia', '291 30th St', Icons.restaurant),
+      ],
+    );
+  }
+
+  ListTile _tile(String title, String subtitle, IconData icon) {
+    return ListTile(
+      title: Text(title,
+          style: const TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 20,
+          )),
+      subtitle: Text(subtitle),
+      leading: Icon(
+        icon,
+        color: Colors.blue[500],
       ),
     );
   }
-  // #enddocregion column
-
-  // #docregion row
-  Widget _buildDecoratedImage(int imageIndex) => Expanded(
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(width: 10, color: Colors.black38),
-            borderRadius: const BorderRadius.all(Radius.circular(8)),
-          ),
-          margin: const EdgeInsets.all(4),
-          child: Image.asset('lib/images/sample$imageIndex.jpeg'),
-        ),
-      );
-
-  Widget _buildImageRow(int imageIndex) => Row(
-        children: [
-          _buildDecoratedImage(imageIndex),
-          _buildDecoratedImage(imageIndex + 1),
-        ],
-      );
-  // #enddocregion row
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+
+// void main() {
+//   debugPaintSizeEnabled = false; // Set to true for visual layout
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter layout demo',
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Flutter layout demo'),
+//         ),
+//         body: Center(child: _buildImageColumn()),
+//       ),
+//     );
+//   }
+
+//   // #docregion column
+//   Widget _buildImageColumn() {
+//     return Container(
+//       decoration: const BoxDecoration(
+//         color: Colors.black26,
+//       ),
+//       child: Column(
+//         children: [
+//           _buildImageRow(1),
+//           _buildImageRow(3),
+//         ],
+//       ),
+//     );
+//   }
+//   // #enddocregion column
+
+//   // #docregion row
+//   Widget _buildDecoratedImage(int imageIndex) => Expanded(
+//         child: Container(
+//           decoration: BoxDecoration(
+//             border: Border.all(width: 10, color: Colors.black38),
+//             borderRadius: const BorderRadius.all(Radius.circular(8)),
+//           ),
+//           margin: const EdgeInsets.all(4),
+//           child: Image.network('https://placeimg.com/300/300/animals'),
+//         ),
+//       );
+
+//   Widget _buildImageRow(int imageIndex) => Row(
+//         children: [
+//           _buildDecoratedImage(imageIndex),
+//           _buildDecoratedImage(imageIndex + 1),
+//         ],
+//       );
+//   // #enddocregion row
+// }
 
 // import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
