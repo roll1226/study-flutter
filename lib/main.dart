7dcb4cb54d56b91e8d46c8934da4044c589cc6a0
roll1226 <tomo_yasu_4724@icloud.com>
2022-04-27 22:46:40 +0900

    fix: auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'todo_list_page.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'controller/auth_controller.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+// import 'package:hooks_riverpod/hooks_riverpod.dart';
+// import 'todo_list_page.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'controller/auth_controller.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(HomePage());
-}
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(MyTodoApp());
+// }
 
 // class MyTodoApp extends StatelessWidget {
 //   @override
@@ -27,22 +27,100 @@ Future<void> main() async {
 //     );
 //   }
 // }
-class HomePage extends ConsumerWidget {
-  const HomePage({Key? key}) : super(key: key);
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final LocalAuthentication auth = LocalAuthentication();
+
+  String _authorized = 'Not Authorized';
+  bool _isAuthenticating = false;
+
+  Future<void> _authenticate() async {
+    bool authenticated = false;
+    try {
+      setState(() {
+        _isAuthenticating = true;
+        _authorized = 'Authenticating';
+      });
+      authenticated = await auth.authenticate(
+          localizedReason: 'Let OS determine authentication method',
+          useErrorDialogs: true,
+          stickyAuth: true);
+      setState(() {
+        _isAuthenticating = false;
+      });
+    } on PlatformException catch (e) {
+      print(e);
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = "Error - ${e.message}";
+      });
+      return;
+    }
+    if (!mounted) return;
+
+    setState(
+        () => _authorized = authenticated ? 'Authorized' : 'Not Authorized');
+  }
+
+  void _cancelAuthentication() async {
+    await auth.stopAuthentication();
+    setState(() => _isAuthenticating = false);
+  }
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('local_auth'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            //生体認証を実行
-            await ref.read(authControllerProvider).didAuthenticate();
-          },
-          child: const Text('生体認証'),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Biometric Authentication'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Current State: $_authorized\n'),
+              (_isAuthenticating)
+                  ? ElevatedButton(
+                      onPressed: _cancelAuthentication,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text("Cancel Authentication"),
+                          Icon(Icons.cancel),
+                        ],
+                      ),
+                    )
+                  : Column(
+                      children: [
+                        ElevatedButton(
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Text('Authenticate'),
+                              Icon(Icons.fingerprint),
+                            ],
+                          ),
+                          onPressed: _authenticate,
+                        ),
+                      ],
+                    ),
+            ],
+          ),
         ),
       ),
     );
